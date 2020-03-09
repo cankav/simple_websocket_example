e4a6afd7ba9e5fdfbead27a158e0073658d2d87e
moita69 <fmj1988@gmail.com>
2020-03-09 20:51:15 -0300

    Update tornado_server.py

--- tornado_server.py
+++ tornado_server.py
@@ -11,39 +11,42 @@ import json
 define('port', type=int, default=8080)
 
 class HelloHandler(tornado.web.RequestHandler):
-  def get(self):
-    self.write('Hello from tornado')
+    def get(self):
+        self.write('Hello from tornado')
 
 class MyWebSocket(tornado.websocket.WebSocketHandler):
   clients = []
 
-  def open(self):
-    # clients must be accessed through class object!!!
-    MyWebSocket.clients.append(self)
-    print ("\nWebSocket opened")
+    def check_origin(self, origin):
+        return True
+    
+    def open(self):
+        # clients must be accessed through class object!!!
+        MyWebSocket.clients.append(self)
+        print ("\nWebSocket opened")
 
-  def on_message(self, message):
-    print ("msg recevied", message)
-    msg = json.loads(message) # todo: safety?
+    def on_message(self, message):
+        print ("msg recevied", message)
+        msg = json.loads(message) # todo: safety?
 
-    # send other clients this message
-    for c in MyWebSocket.clients:
-      if c != self:
-        c.write_message(msg)
+        # send other clients this message
+        for c in MyWebSocket.clients:
+          if c != self:
+            c.write_message(msg)
 
-  def on_close(self):
-    print ("WebSocket closed")
-    # clients must be accessed through class object!!!
-    MyWebSocket.clients.remove(self)
+    def on_close(self):
+        print ("WebSocket closed")
+        # clients must be accessed through class object!!!
+        MyWebSocket.clients.remove(self)
 
 def main():
-  tornado_app = tornado.web.Application([
+    tornado_app = tornado.web.Application([
       ('/hello-tornado', HelloHandler),
       ('/websocket', MyWebSocket),
       ])
-  server = tornado.httpserver.HTTPServer(tornado_app)
-  server.listen(options.port)
-  tornado.ioloop.IOLoop.instance().start()
+    server = tornado.httpserver.HTTPServer(tornado_app)
+    server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
-  main()
+    main()
